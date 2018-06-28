fa9d63501ab75e8eb1cb3896764fe5abe1bd6b30
Jefferson Simão Gonçalves <gerson.simao.92@gmail.com>
2018-06-28 12:23:23 -0300

    Add support Plugin Cakephp 3.6

---
+++ src/Plugin.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Jefferson Simão Gonçalves
+ * Email: gerson.simao.92@gmail.com
+ * Date: 28/06/2018
+ * Time: 12:21
+ */
+
+namespace FieldTypes;
+
+use Cake\Core\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    protected $routesEnabled = false;
+    protected $bootstrapEnabled = false;
+    protected $middlewareEnabled = false;
+    protected $consoleEnabled = false;
+}
